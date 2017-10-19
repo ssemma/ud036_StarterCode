65a4b312402c4f18f077a58e9cbcc07f2a37ffc2
ssemma <shanshanxia59@gmail.com>
2017-10-18 21:51:12 -0400

    working code

--- entertainment_center.py
+++ entertainment_center.py
@@ -25,7 +25,9 @@ conn = httplib.HTTPSConnection("api.themoviedb.org")
 payload = "{}"
 
 # Get the upcoming movie's id, title, poster_path, through request url
-conn.request("GET", "/3/movie/upcoming?page=1&language=en-US&api_key="+API_KEY, payload)
+conn.request("GET",
+             "/3/movie/upcoming?page=1&language=en-US&api_key=" + API_KEY,
+             payload)
 
 
 res = conn.getresponse()
@@ -36,68 +38,50 @@ informations = data.decode("utf-8")
 json_informations = json.loads(informations)
 
 
-#print(json_informations)
-#print(json_informations["results"][1]["poster_path"])
-#print(len(json_informations["results"])) 
-
-
+# print(json_informations)
+# print(json_informations["results"][1]["poster_path"])
+# print(len(json_informations["results"]))
 # iterate data of all movies to obtain their'id, title, poser_image_url
-movies = []
-i = 0
-for i in range(0,len(json_informations["results"])):
-    dictionary_movie= {"title": json_informations["results"][i]["title"],
-                       "id" : json_informations["results"][i]["id"],
-                        "poster_image": "https://image.tmdb.org/t/p/w500" + json_informations["results"][i]["poster_path"]}
-    movies.append(dictionary_movie)
-    #print(movies[i])
-    dictionary_movie ={}
-    i += 1
+for i in range(0, len(json_informations["results"])):
+    dictionary_movie = {"title": json_informations["results"][i]["title"],
+                        "id": json_informations["results"][i]["id"],
+                        "poster_image": "https://image.tmdb.org/t/p/w500" +
+                        json_informations["results"][i]["poster_path"]}
+    movies. append(dictionary_movie)
+    # print(movies[i])
+    dictionary_movie = {}
 
 # Get youtube url for each movie
 for movie in movies:
     # Query the video information for each movie
-    url = "https://api.themoviedb.org/3/movie/" + str(movie["id"])+"/videos?language=en-US&api_key=" + API_KEY
+    url = "https://api.themoviedb.org/3/movie/" + str(movie["id"]) + "/videos?language=en-US&api_key=" + API_KEY
 
-    load="{}"
+    load = "{}"
 
     response = requests.request("GET", url, data=load)
-    #print(response.text)
-    #print(response.text["results"][0]["key"])
+    # print(response.text)
+    # print(response.text["results"][0]["key"])
 
     # JSON the results of all video information for each movie
-    json_video=json.loads(response.text)
-    #print(json_video)
-    #print(json_video["results"][0]["key"])
+    json_video = json.loads(response.text)
+    # print(json_video)
+    # print(json_video["results"][0]["key"])
 
-    # Extract the youtube key and put the trailer_url into each movie's dictionary
+    # Extract the youtube key and put into each movie
     youtube_key = json_video["results"][0]["key"]
-    youtube_url = {"trailer_youtube":"https://www.youtube.com/watch?v="+ youtube_key}
-    movie.update(youtube_url)
-    #print movie["trailer_youtube"]
+    youtube_url = {"trailer_youtube": "https://www.youtube.com/watch?v=" +
+                   youtube_key}
+    movie. update(youtube_url)
+    # print movie["trailer_youtube"]
 
 # Instantiate Each movie
 readymovies = []
 
 for movie in movies:
-    movie["id"] = media.Movie( movie["title"], movie["poster_image"],
-                               movie["trailer_youtube"])
-    #print(movie["title"])
-    readymovies.append(movie["id"])
-    
+    movie["id"] = media.Movie(movie["title"], movie["poster_image"],
+                  movie["trailer_youtube"])
+    # print(movie["title"])
+    readymovies.append(movie["id"])  
+# print readymovies
 
-#print readymovies
-
-fresh_tomatoes.open_movies_page(readymovies) 
-
-
-    
-
-
-
-
-
-
-
-
-
-
+fresh_tomatoes.open_movies_page(readymovies)
