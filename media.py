ac8915734998c1b419e1906b7d4d06acea17e069
ssemma <shanshanxia59@gmail.com>
2017-10-19 11:23:11 -0400

    final final with key

--- media.py
+++ media.py
@@ -1,10 +1,6 @@
-
-
 class Movie():
     """This class provide a way to store movie related information"""
-    
-    def __init__(self, movie_title,poster_image,trailer_youtube):
-        
+    def __init__(self, movie_title, poster_image, trailer_youtube):
         self.title = movie_title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
