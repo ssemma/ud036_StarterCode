3be7bcac8c36a81d047856c2640ab245f7cb0116
ssemma <shanshanxia59@gmail.com>
2017-10-20 08:28:16 -0400

    solved bottom padding issue

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -19,6 +19,7 @@ main_page_head = '''
     <style type="text/css" media="screen">
         body {
             padding-top: 80px;
+            padding-bottom:80px;
         }
         #trailer .modal-dialog {
             margin-top: 200px;
